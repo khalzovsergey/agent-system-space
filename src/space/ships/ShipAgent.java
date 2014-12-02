e0bfc8aab3b6046c333431d92dcc9d8b59ea0d5a
Sergey Khalzov <khalzovsergey@mail.ru>
2014-12-02 20:48:49 +0300

    Наброски планеты

---
+++ src/space/ships/ShipAgent.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space.ships;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author Sergey
+ */
+public class ShipAgent extends Agent
+{
+    protected void setup()
+    {
+    }
+    
+    protected void takeDown()
+    {
+        System.exit(0);
+    }
+}
